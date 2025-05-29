00c2a9a9a60fbce808a5b1d2fe4acb5a376ed109
Friedel Schön <derfriedmundschoen@gmail.com>
2025-05-30 00:02:43 +0200

    interface might be '*'

--- component/netspeed.go
+++ component/netspeed.go
@@ -25,8 +25,8 @@ func NetspeedRx(interfaceName string, _ *notify.Notification, cacheptr *any) (st
 
 	var rx uint64
 	for _, s := range stats {
-		if s.Name == interfaceName {
-			rx = s.BytesRecv
+		if interfaceName == "*" || s.Name == interfaceName {
+			rx += s.BytesRecv
 			break
 		}
 	}
@@ -53,8 +53,8 @@ func NetspeedTx(interfaceName string, _ *notify.Notification, cacheptr *any) (st
 
 	var tx uint64
 	for _, s := range stats {
-		if s.Name == interfaceName {
-			tx = s.BytesSent
+		if interfaceName == "*" || s.Name == interfaceName {
+			tx += s.BytesSent
 			break
 		}
 	}
